ab235f91f915780b824d99b80f6e8f2170590d71
Marlon <marloncs13@outlook.com>
2021-10-18 15:21:50 -0300

    adicionando Template Default, Header, UserDashbord

---
+++ pages/user/dashbord.js
@@ -0,0 +1,141 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from '@mui/material'
+import { makeStyles } from '@mui/styles'
+import TemplateDefault from '../../src/templates/Default'
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  buttonAdd: {
+    margin: '30px auto',
+    display: 'block',
+  },
+  cardMedia: {
+    paddingTop: '56%',
+  },
+}))
+
+const Dashbord = () => {
+  const classes = useStyles()
+  return (
+    <>
+      <TemplateDefault>
+        <Container maxWidth="sm" className={classes.container}>
+          <Typography component="h1" variant="h2" align="center">
+            Meus Anuncios
+          </Typography>
+          <Button variant="outlined" color="primary" className={classes.buttonAdd}>
+            Publicar novo anuncio
+          </Button>
+        </Container>
+        <Container maxWidth="md">
+          <Grid container spacing={4}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  image={'https://source.unsplash.com/random'}
+                  title="Titulo da imagem"
+                  className={classes.cardMedia}
+                />
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    Produto X
+                  </Typography>
+                  <Typography>R$ 60,00</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary" variant="outlined">
+                    Editar
+                  </Button>
+                  <Button size="small" color="primary" variant="outlined">
+                    Excluir
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  image={'https://source.unsplash.com/random'}
+                  title="Titulo da imagem"
+                  className={classes.cardMedia}
+                />
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    Produto X
+                  </Typography>
+                  <Typography>R$ 60,00</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary" variant="outlined">
+                    Editar
+                  </Button>
+                  <Button size="small" color="primary" variant="outlined">
+                    Excluir
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  image={'https://source.unsplash.com/random'}
+                  title="Titulo da imagem"
+                  className={classes.cardMedia}
+                />
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    Produto X
+                  </Typography>
+                  <Typography>R$ 60,00</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary" variant="outlined">
+                    Editar
+                  </Button>
+                  <Button size="small" color="primary" variant="outlined">
+                    Excluir
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  image={'https://source.unsplash.com/random'}
+                  title="Titulo da imagem"
+                  className={classes.cardMedia}
+                />
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    Produto X
+                  </Typography>
+                  <Typography>R$ 60,00</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary" variant="outlined">
+                    Editar
+                  </Button>
+                  <Button size="small" color="primary" variant="outlined">
+                    Excluir
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </TemplateDefault>
+    </>
+  )
+}
+
+export default Dashbord
---
+++ src/templates/Default.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+const Default = ({ children }) => {
+  return (
+    <>
+      <Header>Header</Header>
+      {children}
+      <footer>Footer</footer>
+    </>
+  )
+}
+
+export default Default
