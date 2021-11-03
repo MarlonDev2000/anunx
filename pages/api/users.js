4bf1e20714f30768289305828fcfe11c3ca0776e
Marlon <marloncs13@outlook.com>
2021-11-03 18:08:51 -0300

    refactor de rotas usando next connect

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,28 +1,10 @@
-import dbConnect from '../../src/utils/dbConnect'
-import { crypto } from '../../src/utils/password'
-import UsersModel from '../../src/models/users'
+import nextConnect from 'next-connect'
+import { get, post } from '../../src/controllers/users'
 
-const users = async (req, res) => {
-  const { method } = req
-  switch (method) {
-    case 'GET':
-      await dbConnect()
-      res.status(200).json({ success: true })
-      break
-    case 'POST':
-      const { name, email, password } = req.body
-      await dbConnect()
-      const passwordCrypto = await crypto(password)
-      const user = new UsersModel({
-        name: name,
-        email: email,
-        password: passwordCrypto,
-      })
+const route = nextConnect()
 
-      user.save()
+route.get(get)
 
-      res.status(201).json({ success: true })
-  }
-}
+route.post(post)
 
-export default users
+export default route
